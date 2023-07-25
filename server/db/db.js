0b6e989cadc80d02b224412ebb0a8436b6c355cd
Brian Ngo <brianhngo@gmail.com>
2023-07-25 08:14:27 -0700

    Deployment finished

--- server/db/db.js
+++ server/db/db.js
@@ -20,7 +20,7 @@ if (process.env.DATABASE_URL) {
   // //   },
   // };
 }
-console.log(process.env.DATABASE_URL);
+
 const db = new Sequelize(
   // process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   process.env.DATABASE_URL,
